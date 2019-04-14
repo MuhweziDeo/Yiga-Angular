501e3e75f9a61103a67a8190835a91044697994d
muhwezi deo <aggrey256@gmail.com>
2019-04-14 19:13:24 +0300

    ngClass

--- Basics/my-first-app/src/app/server/server.component.ts
+++ Basics/my-first-app/src/app/server/server.component.ts
@@ -2,11 +2,14 @@ import { Component, } from '@angular/core';
 
 @Component({
 selector: 'app-server',
-templateUrl: './server.component.html'
+templateUrl: './server.component.html',
+styles: [`
+.ONLINE { color: 'white' }
+`]
 })
 export class ServerComponent {
 serverId: number = 10;
-serverStatus: string = 'Running';
+serverStatus: string = '';
 
 constructor(){
     this.serverStatus = Math.random()>0.5 ? 'ONLINE':'OFFLINE';
