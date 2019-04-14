3cc802495079db3f3f4b090a470a121e79fb6f4f
muhwezi deo <aggrey256@gmail.com>
2019-04-14 14:47:46 +0300

    selector naming

--- Basics/my-first-app/src/app/servers/servers.component.ts
+++ Basics/my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // select by attribute
+  // selector: '[app-servers]',
+  // select by class
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
