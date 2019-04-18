d66f3bf953480dc921350e43d97f218593525d70
muhwezi deo <aggrey256@gmail.com>
2019-04-18 19:23:37 +0300

    display single recipe

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipes.model';
 
 @Component({
@@ -7,14 +7,22 @@ import {Recipe} from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe [] = [
   new Recipe('dees', 'A new one', 'https://images.pexels.com/' +
+    'photos/1166419/pexels-photo-1166419.jpeg?cs=srgb&dl=' +
+    'avocado-cherry-tomatoes-chopping-board-1166419.jpg&fm=jpgΩ'),
+  new Recipe('dee2', 'A new one 2', 'https://images.pexels.com/' +
     'photos/1166419/pexels-photo-1166419.jpeg?cs=srgb&dl=' +
     'avocado-cherry-tomatoes-chopping-board-1166419.jpg&fm=jpgΩ')
 ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipeElement: Recipe) {
+    this.recipeWasSelected.emit(recipeElement);
   }
 
 }
