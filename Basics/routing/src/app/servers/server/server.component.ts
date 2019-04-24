c1274cec5c039be9d20c92dbddbb98990f9096fe
muhwezi deo <aggrey256@gmail.com>
2019-04-24 20:47:14 +0300

    routing

---
+++ Basics/routing/src/app/servers/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import serve from "@angular/cli/commands/serve";
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute ) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    console.log(this.server)
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
+  }
+
+}
